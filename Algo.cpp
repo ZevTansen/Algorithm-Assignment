d218ce4afc91ab6ff7e3d2b3f897d478c865b6b4
Md. Tansen Uddin Tansu <tansenuddin33671@gmail.com>
2026-03-26 20:22:04 +0600

    Initial commit - sorting algorithms assignment

---
+++ Algo.cpp
@@ -0,0 +1,141 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> generateData(int n) {
+    vector<int> arr(n);
+
+    for(int i = 0; i < n; i++) {
+        arr[i] = rand() % 100000;
+    }
+
+    return arr;
+}
+
+void insertionSort(vector<int>& arr) {
+    int n = arr.size();
+
+    for(int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+
+        while(j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        arr[j + 1] = key;
+    }
+}
+
+
+void merge(vector<int>& arr, int left, int mid, int right) {
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    vector<int> L(n1), R(n2);
+
+    for (int i = 0; i < n1; i++)
+        L[i] = arr[left + i];
+    for (int j = 0; j < n2; j++)
+        R[j] = arr[mid + 1 + j];
+
+    int i = 0, j = 0, k = left;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        } else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1) {
+        arr[k] = L[i];
+        i++; k++;
+    }
+
+    while (j < n2) {
+        arr[k] = R[j];
+        j++; k++;
+    }
+}
+
+
+void mergeSort(vector<int>& arr, int left, int right) {
+    if (left >= right)
+        return;
+
+    int mid = left + (right - left) / 2;
+    mergeSort(arr, left, mid);
+    mergeSort(arr, mid + 1, right);
+    merge(arr, left, mid, right);
+}
+
+
+int partition(vector<int>& arr, int low, int high) {
+    int pivot = arr[high];  
+    int i = low - 1;        
+
+    for (int j = low; j < high; j++) {
+        if (arr[j] <= pivot) {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[i + 1], arr[high]);
+    return i + 1;
+}
+
+
+void quickSort(vector<int>& arr, int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+int main(){
+    cout << "Student Name: Tansen Nurmagomedov\n";
+    cout << "Student ID: 123456\n\n";
+
+  srand(time(0)); 
+
+    vector<int> sizes = {10, 100, 1000, 10000, 100000};
+
+    for (int n : sizes) {
+        cout << "Dataset size: " << n << endl;
+
+        
+        vector<int> original = generateData(n);
+
+        
+        vector<int> arrInsertion = original;
+        auto startIns = chrono::high_resolution_clock::now();
+        insertionSort(arrInsertion);
+        auto endIns = chrono::high_resolution_clock::now();
+        auto durationIns = chrono::duration_cast<chrono::microseconds>(endIns - startIns);
+        cout << "Insertion Sort Time: " << durationIns.count() << " microseconds" << endl;
+
+        
+        vector<int> arrMerge = original;
+        auto startMerge = chrono::high_resolution_clock::now();
+        mergeSort(arrMerge, 0, arrMerge.size() - 1);
+        auto endMerge = chrono::high_resolution_clock::now();
+        auto durationMerge = chrono::duration_cast<chrono::microseconds>(endMerge - startMerge);
+        cout << "Merge Sort Time: " << durationMerge.count() << " microseconds" << endl;
+
+        
+        vector<int> arrQuick = original;
+        auto startQuick = chrono::high_resolution_clock::now();
+        quickSort(arrQuick, 0, arrQuick.size() - 1);
+        auto endQuick = chrono::high_resolution_clock::now();
+        auto durationQuick = chrono::duration_cast<chrono::microseconds>(endQuick - startQuick);
+        cout << "Quick Sort Time: " << durationQuick.count() << " microseconds" << endl;
+
+        cout << "------------------------" << endl;
+    }
+    return 0;
+}
