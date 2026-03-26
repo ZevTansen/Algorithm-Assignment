98e9c80626f84da8404b11a4d28ce9c83c5ee1b0
Md. Tansen Uddin Tansu <tansenuddin33671@gmail.com>
2026-03-26 21:10:56 +0600

    I changed the code to get the runtime result in seconds

--- Algo.cpp
+++ Algo.cpp
@@ -98,10 +98,10 @@ void quickSort(vector<int>& arr, int low, int high) {
 }
 
 int main(){
-    cout << "Student Name: Tansen Nurmagomedov\n";
-    cout << "Student ID: 123456\n\n";
+    cout << "Student Name: Md. Tansen Uddin\n";
+    cout << "Student ID: C243012\n\n";
 
-  srand(time(0)); 
+    srand(time(0)); 
 
     vector<int> sizes = {10, 100, 1000, 10000, 100000};
 
@@ -116,26 +116,27 @@ int main(){
         auto startIns = chrono::high_resolution_clock::now();
         insertionSort(arrInsertion);
         auto endIns = chrono::high_resolution_clock::now();
-        auto durationIns = chrono::duration_cast<chrono::microseconds>(endIns - startIns);
-        cout << "Insertion Sort Time: " << durationIns.count() << " microseconds" << endl;
+        chrono::duration<double> durationIns = endIns - startIns; // seconds
+        cout << "Insertion Sort Time: " << durationIns.count() << " seconds" << endl;
 
         
         vector<int> arrMerge = original;
         auto startMerge = chrono::high_resolution_clock::now();
         mergeSort(arrMerge, 0, arrMerge.size() - 1);
         auto endMerge = chrono::high_resolution_clock::now();
-        auto durationMerge = chrono::duration_cast<chrono::microseconds>(endMerge - startMerge);
-        cout << "Merge Sort Time: " << durationMerge.count() << " microseconds" << endl;
+        chrono::duration<double> durationMerge = endMerge - startMerge; // seconds
+        cout << "Merge Sort Time: " << durationMerge.count() << " seconds" << endl;
 
         
         vector<int> arrQuick = original;
         auto startQuick = chrono::high_resolution_clock::now();
         quickSort(arrQuick, 0, arrQuick.size() - 1);
         auto endQuick = chrono::high_resolution_clock::now();
-        auto durationQuick = chrono::duration_cast<chrono::microseconds>(endQuick - startQuick);
-        cout << "Quick Sort Time: " << durationQuick.count() << " microseconds" << endl;
+        chrono::duration<double> durationQuick = endQuick - startQuick; // seconds
+        cout << "Quick Sort Time: " << durationQuick.count() << " seconds" << endl;
 
         cout << "------------------------" << endl;
     }
+
     return 0;
 }
